0e15d83f1d13141e0851e049f9226d6395d3e664
Heesung Youn <heesung6701@naver.com>
2020-07-10 22:01:07 +0900

    ADD Validation on Controller

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,7 +45,10 @@ public class DesignTacoController {
 	}
 	
 	@PostMapping
-	public String processDesign(Taco design) {
+	public String processDesign(Taco design, Errors errors) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
 		log.info("Processing design: " + design);
 		return "redirect:/orders/current";
 	}
