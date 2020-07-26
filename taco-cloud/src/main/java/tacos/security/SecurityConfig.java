65b155c7bc617a01413ad30909677104f3f4c71e
Heesung Youn <heesung6701@naver.com>
2020-07-26 23:36:31 +0900

    Update SecurityConfig to JdbcAuth

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,14 +1,21 @@
 package tacos.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource; 
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -24,13 +31,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("user1")
-			.password("{noop}password1")
-			.authorities("ROLE_USER")
-			.and()
-			.withUser("user2")
-			.password("{noop}password2")
-			.authorities("ROLE_USER");
+		auth
+			.jdbcAuthentication()
+		    .dataSource(dataSource)
+		    .usersByUsernameQuery("select username, password, enabled from users where username=?")
+		    .authoritiesByUsernameQuery("select username, authority from authorities where username = ?")
+		    .passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
