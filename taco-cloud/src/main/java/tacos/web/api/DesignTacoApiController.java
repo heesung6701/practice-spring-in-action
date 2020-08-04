a97196ce70bfa521dee7914c9c2b0aa05fbc007e
Heesung Youn <heesung6701@naver.com>
2020-08-04 22:24:12 +0900

    ADD DesignTacoApiController

---
+++ taco-cloud/src/main/java/tacos/web/api/DesignTacoApiController.java
@@ -0,0 +1,47 @@
+package tacos.web.api;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+@RestController
+@RequestMapping(path="design", produces="application/json")
+@CrossOrigin(origins="*")
+public class DesignTacoApiController {
+	private TacoRepository tacoRepo;
+	
+	@Autowired
+	EntityLinks entityLinks;
+	
+	public DesignTacoApiController(TacoRepository tacoRepo) {
+		this.tacoRepo = tacoRepo;
+	}
+	
+	@GetMapping("/recent")
+	public Iterable<Taco> recentTacos(){
+		PageRequest page = PageRequest.of(0,  12, Sort.by("createdAt").descending());
+		return tacoRepo.findAll(page).getContent();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Taco> tacoByid(@PathVariable("id") Long id) {
+		Optional<Taco> optTaco = tacoRepo.findById(id);
+		if(optTaco.isPresent()) {
+			return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+	}	
+}
